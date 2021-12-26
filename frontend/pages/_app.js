0a503239ec81bfef4e256e30558259cac4ec8d62
ana sanots <anasoliveiraaa@gmail.com>
2021-12-26 13:39:50 -0500

    ServerStyleSheet added

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,16 @@
-import Page from '../components/Page'
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import Page from '../components/Page';
+import '../components/styles/nprogress.css';
 
-export default function MyApp({Component, pageProps}){
-    return <Page>
-        <Component {...pageProps}/>
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Page>
+      <Component {...pageProps} />
     </Page>
+  );
 }
-
--- frontend/pages/account.js
+++
@@ -1,4 +0,0 @@
-export default function AccountPage() {
-    return <p>hello</p>;
-  }
-  
