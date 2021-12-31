78ca977b454b0345468039802b7a9bfb41fad7d7
ana sanots <anasoliveiraaa@gmail.com>
2021-12-31 15:29:53 -0500

    improved navbar

--- frontend/components/Nav.jsx
+++ frontend/components/Nav.jsx
@@ -1,13 +1,24 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import { useUser } from './User';
 
 export default function Nav() {
+  const user = useUser();
   return (
     <NavStyles>
       <Link href="/products">Products</Link>
-      <Link href="/sell">Sell</Link>
-      <Link href="/orders">Orders</Link>
-      <Link href="/account">Account</Link>
+      {user && ( //if there's a user then show these tabs
+        <>
+          <Link href="/sell">Sell</Link>
+          <Link href="/orders">Orders</Link>
+          <Link href="/account">Account</Link>
+        </>
+      )}
+      {!user && ( // if no user then it has to sign in
+        <>
+          <Link href="/signin">Sign In</Link>
+        </>
+      )}
     </NavStyles>
   );
 }
